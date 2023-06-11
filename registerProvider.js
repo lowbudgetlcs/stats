e6c9a36ddb59048ab0ff432e6d1790be89a92cf8
ruuffian <ruuffia@gmail.com>
2023-06-11 07:30:21 -0400

    Converted registerProvider to axios from fetch()

--- registerProvider.js
+++ registerProvider.js
@@ -1,17 +1,26 @@
-require("dotenv").config();
-
-const body = {
-"region": "NA",
-"url": "https://us-central1-lblcs-389419.cloudfunctions.net/captureMatchDTO"
-};
-
-const res = await fetch("https://americas.api.riotgames.com/lol/tournament/v4/providers", {
-    method: 'POST',
+require('dotenv').config();
+const Axios = require('axios');
+const axios = Axios.create({
+    baseURL: 'https://americas.api.riotgames.com/',
     headers: {
-        "X-Riot-Token": process.env.TOKEN,
+        'X-Riot-Token': process.env.TOKEN,
         'Content-Type': 'application/json',
     },
-    body: JSON.stringify(body),
 });
 
-console.log(res);
+const body = {
+    'region': 'NA',
+    'url': 'https://us-central1-lblcs-389419.cloudfunctions.net/captureMatchDTO',
+};
+
+const url = 'lol/tournament/v4/providers';
+
+try {
+    const res = async () => await axios.post(url, {
+        data: body,
+    });
+    console.log(`Recieved response ${res.status} with body ${res.data}`);
+}
+catch (e) {
+    console.log(e);
+}
