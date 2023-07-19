e8e6aadab442af9cff8cf52e35da6d4a8a129fe2
ruuffian <ruuffia@gmail.com>
2023-07-19 18:45:00 -0400

    Begin sheet API portion

--- captureMatchDTO.js
+++ captureMatchDTO.js
@@ -10,12 +10,13 @@ const log = logging.log(logName);
 let entry;
 
 const targetURLs = {
-    TEST: process.env.TEST_URL,
+    TEST: process.env.TEST_SHEET_ID,
 };
 
 exports.captureMatchDTO = async (req, res) => {
     entry = log.entry(`Recieved match data at ${new Date().toTimeString()}`);
     log.info(entry);
+    res.status(200).send();
     const client = axios.create({
         baseURL: 'https://americas.api.riotgames.com',
         headers: {
@@ -26,38 +27,38 @@ exports.captureMatchDTO = async (req, res) => {
             return status >= 200 && status <= 299;
         },
     });
-    res.status(200).send();
+
     const gameId = req.body.gameId;
     const target = req.body.metaData;
     if (!(gameId && target)) {
-        entry = log.entry(`Missing gameId or target`);
+        entry = log.entry('Missing gameId or target');
         log.warning(entry);
         return;
     }
-    // Acknowledge payload delivery
-    const participants = await getMatchV5(gameId);
-    if(!participants){
-        entry = log.entry(`Missing participant data`);
+    const participants = await getMatchV5(client, gameId);
+    if (!participants) {
+        entry = log.entry('Missing participant data');
         log.error(entry);
         return;
     }
+    // Replace puuid's with player names
     const players = await participantDTOHandler(participants);
-    // Send data to google script for collection
+    // Append data to stat sheet
     const resGoogle = await client.post(targetURLs[target],
-    {
-                'players': players,
-            });
-        entry = log.entry(`Recieved ${resGoogle.status} from ${target} endpoint`);
-        log.info(entry);
-    }
+        {
+            'players': players,
+        });
+    entry = log.entry(`Recieved ${resGoogle.status} from ${target} endpoint`);
+    log.info(entry);
 };
 
-async function getMatchV5(){
+
+async function getMatchV5(client, gameId) {
     try {
         const matchV5Response = await client.get(`/lol/match/v5/matches/NA1_${gameId}`);
         entry = log.entry(`Recieved ${matchV5Response.status} from match-v5 endpoint`);
         log.info(entry);
-        const matchDTO = await matchV5Response.data;
+        const matchDTO = matchV5Response.data;
         return matchDTO.info.participants;
     }
     catch (e) {
@@ -74,6 +75,7 @@ async function getMatchV5(){
 async function participantDTOHandler(participants) {
     entry = log.entry(`Participant data prep started @${new Date().toTimeString()}`);
     log.info(entry);
+    // New client for SUMMONER endpoint
     const client = axios.create({
         baseURL: 'https://na1.api.riotgames.com',
         headers: {
@@ -84,7 +86,8 @@ async function participantDTOHandler(participants) {
             return status >= 200 && status <= 299;
         },
     });
-    const playerDTO = [];
+
+    const playerData = [];
     for (const participant of participants) {
         const data = {};
         try {
@@ -94,25 +97,58 @@ async function participantDTOHandler(participants) {
             log.info(entry);
             const name = res.data.name;
             data.name = name;
+            // Extract relevant data
+            data.kills = participant.kills;
+            data.deaths = participant.deaths;
+            data.assists = participant.assists;
+            data.kda = participant.challenges.kda;
+            data.level = participant.champLevel;
+            data.championName = participant.championName;
+            data.transform = participant.Transform;
+            data.firstBlood = participant.firstBloodKill;
+            data.firstTower = participant.firstTowerKill;
+            data.gold = participant.goldEarned;
+            data.visionScore = participant.visionScore;
+            playerData.push(data);
         }
         catch (e) {
             axiosError(e);
         }
-        // Extract relevant data
-        data.kills = participant.kills;
-        data.deaths = participant.deaths;
-        data.assists = participant.assists;
-        data.kda = participant.challenges.kda;
-        data.level = participant.champLevel;
-        data.championName = participant.championName;
-        data.transform = participant.Transform;
-        data.firstBlood = participant.firstBloodKill;
-        data.firstTower = participant.firstTowerKill;
-        data.gold = participant.goldEarned;
-        data.visionScore = participant.visionScore;
-        playerDTO.push(data);
     }
-    return playerDTO;
+    return playerData;
+}
+
+async function appendValues(spreadsheetId, range, valueInputOption, _values){
+    const {GoogleAuth} = require('google-auth-library');
+  const {google} = require('googleapis');
+
+  const auth = new GoogleAuth({
+    scopes: 'https://www.googleapis.com/auth/spreadsheets',
+  });
+
+  const service = google.sheets({version: 'v4', auth});
+  let values = [
+    [
+      // Cell values ...
+    ],
+    // Additional rows ...
+  ];
+  const resource = {
+    values,
+  };
+  try {
+    const result = await service.spreadsheets.values.append({
+      spreadsheetId,
+      range,
+      valueInputOption,
+      resource,
+    });
+    console.log(`${result.data.updates.updatedCells} cells appended.`);
+    return result;
+  } catch (err) {
+    // TODO (developer) - Handle exception
+    throw err;
+  }
 }
 
 function axiosError(err) {
